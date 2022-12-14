35ce7834083dea9e6723969a1a9369d008929db7
natali <devidvarl@gmail.com>
2022-12-14 14:12:57 +0100

    format selection

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 class Animal {
     constructor(name) {
-      this.name = name;
+        this.name = name;
     }
-  }
-
-  class Rabbit extends Animal {
+}
+class Rabbit extends Animal {
     constructor(name) {
-      super(name);
-      this.created = Date.now();
+        super(name);
+        this.created = Date.now();
     }
-  }
-  let rabbit = new Rabbit("White Rabbit");
-  alert(rabbit.name);
+}
+let rabbit = new Rabbit("White Rabbit");
+alert(rabbit.name);
